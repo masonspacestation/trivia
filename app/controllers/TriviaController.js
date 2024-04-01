ddd9d8f15ec65828efe92064f97c09ae5138784d
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-04-01 16:16:24 -0600

    all questions and incorrect answers displaying. next, display single set.

--- app/controllers/TriviaController.js
+++ app/controllers/TriviaController.js
@@ -1,5 +1,7 @@
 import { AppState } from "../AppState.js";
+import { TriviaQuestion } from "../models/TriviaQuestion.js";
 import { triviasService } from "../services/TriviaService.js";
+import { setHTML } from "../utils/Writer.js";
 
 
 export class TriviaController {
@@ -8,7 +10,9 @@ export class TriviaController {
     console.log('🎮');
     // this.connectionTest()
     this.getTrivia()
-
+    this.drawQuestion
+    AppState.on('triviaQuestions', this.drawQuestion)
+    AppState.on('triviaQuestions', this.drawAnswers)
   }
 
   getTrivia() {
@@ -16,7 +20,28 @@ export class TriviaController {
 
   }
 
+  drawQuestion() {
+    // this will show the question, wait a few seconds, then drawAnswers()
+    let questions = AppState.triviaQuestions
+    let questionDisplay = ''
+    questions.forEach(question => questionDisplay += question.activeQuestion)
+    setHTML('question-display', questionDisplay)
 
+  }
+
+
+  drawAnswers() {
+    let answers = AppState.triviaQuestions
+    let answersDisplay = ''
+    answers.forEach(answer => answersDisplay += answer.activeIncorrectAnswers)
+    setHTML('answers-display', answersDisplay)
+
+  }
+
+
+  revealCorrectAnswer() {
+
+  }
 
   // connectionTest() {
   //   triviasService.connectionTest('passed')
--- app/models/TriviaQuestion.js
+++ app/models/TriviaQuestion.js
@@ -4,13 +4,50 @@ export class TriviaQuestion {
     this.type = data.type
     this.question = data.question
     this.correctAnswer = data.correct_answer
-    this.incorrectAnswer = data.incorrect_answer
+    this.incorrectAnswer = data.incorrect_answers
     this.difficulty = data.difficulty
     this.category = data.category
   }
 
+  get activeQuestion() {
+    return `
+    <div class="card col-6 mb-5">
+      <h4>${this.question}</h4>
+      <div class="d-flex justify-content-around">
+        <span class="w-auto text-start">Difficulty: ${this.difficulty}</span><span class="w-auto text-end">Category: ${this.category}</span>
+      </div>
+    </div>
+  </section>
+`
+  }
 
+  get activeAnswers() {
+    return `
+    <div class="col-4 border border-4 bg-light">
+      <h3>${this.correctAnswer}</h3>
+    </div>
+    <div class="col-4 border border-4 bg-dark text-light">
+      <h3>${this.incorrectAnswer}</h3>
+    </div>
+    <div class="col-4 border border-4 bg-dark text-light">
+      <h3>${this.incorrectAnswer}</h3>
+    </div>
+    <div class="col-4 border border-4 bg-light">
+      <h3>${this.incorrectAnswer}</h3>
+    </div>
+  </section>
+  `
+  }
 
+  get activeIncorrectAnswers() {
+    let activeIncorrectAnswers = ''
+    this.incorrectAnswer.forEach(incAnswer => activeIncorrectAnswers += `
+    <div class="col-4 border border-4 bg-dark text-light">
+    <h3>${incAnswer}</h3>
+  </div>
+  `)
+    return activeIncorrectAnswers
+  }
 
 }
 
